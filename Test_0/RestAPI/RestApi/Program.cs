090a4d330adc16bae76a958c410ca4e005dc1e14
Marlon <marlon.lagua@merlot.aero>
2024-04-23 07:08:22 +0800

    Help Docs

--- Test_0/RestAPI/RestApi/Program.cs
+++ Test_0/RestAPI/RestApi/Program.cs
@@ -28,3 +28,27 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+
+/**************************** COMPONENTS ********************
+--- Create Project
+dotnet new webapi --use-controllers -o RestApi
+cd RestApi
+dotnet add package Microsoft.EntityFrameworkCore.InMemory
+code -r ../RestApi
+
+--- Certificate
+dotnet dev-certs https --trust
+
+-- execute
+dotnet run --launch-profile https
+
+--- CODE GENERATOR
+dotnet add package Microsoft.VisualStudio.Web.CodeGeneration.Design
+dotnet add package Microsoft.EntityFrameworkCore.Design
+dotnet add package Microsoft.EntityFrameworkCore.SqlServer
+dotnet add package Microsoft.EntityFrameworkCore.Tools
+dotnet tool uninstall -g dotnet-aspnet-codegenerator
+dotnet tool install -g dotnet-aspnet-codegenerator
+dotnet tool update -g dotnet-aspnet-codegenerator
+*************************************************************/
