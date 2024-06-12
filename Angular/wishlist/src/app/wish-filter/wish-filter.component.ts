c6925fbf57e6446a00beb80217d6a73e45f7ac2f
daisuke <lagua.marlon@gmail.com>
2024-06-13 05:52:56 +0800

    Wishlist Project from Tutorial

---
+++ Angular/wishlist/src/app/wish-filter/wish-filter.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { WishItem } from '../../shared/models/wishitem';
+
+const filters = [
+  (item : WishItem) => item,
+  (item : WishItem) => !item.isComplete,
+  (item : WishItem) => item.isComplete
+]
+
+@Component({
+  selector: 'wish-filter',
+  standalone: true,
+  imports: [],
+  templateUrl: './wish-filter.component.html',
+  styleUrl: './wish-filter.component.css'
+})
+export class WishFilterComponent implements OnInit {
+  @Input() filter: any;
+  @Output() filterChange = new EventEmitter<any>();
+  constructor() {}
+  ngOnInit(): void {
+    this.updateFilter('0');
+  }
+
+  listFilter : any = '0';
+
+  updateFilter(value : any) {
+    this.filter = filters[value];
+    this.filterChange.emit(this.filter);
+  }
+}
