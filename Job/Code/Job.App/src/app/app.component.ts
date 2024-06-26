5fb57d642bf6218e1e5f293c182c9b3d48c03cb4
daisuke <lagua.marlon@gmail.com>
2024-06-27 07:12:00 +0800

    css cleanup

--- Job/Code/Job.App/src/app/app.component.ts
+++ Job/Code/Job.App/src/app/app.component.ts
@@ -11,4 +11,18 @@ import { RouterModule } from '@angular/router';
 })
 export class AppComponent {
   title = 'job.app';
+  navBars = new Array(4).fill(false);
+
+  setSelected(selectedValue: string) {
+    this.navBars.fill(false);
+    if (selectedValue == "home") {
+      this.navBars[0] = true;
+    } else if (selectedValue == "application") {
+      this.navBars[1] = true;
+    } else if (selectedValue == "location") {
+      this.navBars[2] = true;
+    } else if (selectedValue == "jobstatus") {
+      this.navBars[3] = true;
+    }
+  }
 }
--- Job/Code/Job.App/src/app/home-skill-item/home-skill-item.component.ts
+++ Job/Code/Job.App/src/app/home-skill-item/home-skill-item.component.ts
@@ -1,9 +1,10 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'home-skill-item',
   standalone: true,
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './home-skill-item.component.html',
   styleUrl: './home-skill-item.component.css'
 })
