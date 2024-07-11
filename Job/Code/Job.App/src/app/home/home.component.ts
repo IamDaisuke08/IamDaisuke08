c4915dfd846aaeccbe90d1a9cc76a9cc2269c751
daisuke <lagua.marlon@gmail.com>
2024-07-12 07:21:19 +0800

    contact me: initial

--- Job/Code/Job.App/src/app/home/home.component.ts
+++ Job/Code/Job.App/src/app/home/home.component.ts
@@ -3,11 +3,18 @@ import { HomeHeroComponent } from '../home-hero/home-hero.component';
 import { HomeSkillListComponent } from '../home-skill-list/home-skill-list.component';
 import { HomeSoftSkillsComponent } from '../home-soft-skills/home-soft-skills.component';
 import { HomeHobiesComponent } from '../home-hobies/home-hobies.component';
+import { ContactMeComponent } from '../contact-me/contact-me.component';
 
 @Component({
   selector: 'home',
   standalone: true,
-  imports: [HomeHeroComponent, HomeSkillListComponent, HomeSoftSkillsComponent, HomeHobiesComponent],
+  imports: [
+    HomeHeroComponent, 
+    HomeSkillListComponent, 
+    HomeSoftSkillsComponent,
+    HomeHobiesComponent,
+    ContactMeComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
